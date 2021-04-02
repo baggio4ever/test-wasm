b36b855a581729237a2fa2e03ae21b169d03db99
baggio4ever <baggio4ever@gmail.com>
2021-04-02 15:49:58 +0900

    整形

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-declare var Module:any;
-declare var ModuleHoge:any;
+declare var Module: any;
+declare var ModuleHoge: any;
 
 @Component({
   selector: 'app-root',
@@ -12,35 +12,28 @@ export class AppComponent {
   title = 'test-wasm';
   instance = null;
 
-  click1():void {
+  click1(): void {
     console.log('押されたよ');
-    
+
     var ModuleHogeInstance = typeof ModuleHogeInstance !== 'undefined' ? ModuleHogeInstance : {};
     fetch('../assets/test1.wasm')
-    .then(response => response.arrayBuffer())
-    .then(buffer => new Uint8Array(buffer))
-    .then(binary => {
-    var moduleArgs = {
-    wasmBinary: binary,
-    onRuntimeInitialized: function () {
-    console.log('initialized');
-    }
-    };
-    ModuleHogeInstance = ModuleHoge(moduleArgs);
-    ModuleHogeInstance.then((m)=>{
-      this.instance = m;
-    });
-    });
-    /*
-    var v1 = this.multiply(5,20);
-    var v2 = this.getLength('abcdef');
-
-    console.log(`v1: ${v1}`);
-    console.log(`v2: ${v2}`);
-    */
+      .then(response => response.arrayBuffer())
+      .then(buffer => new Uint8Array(buffer))
+      .then(binary => {
+        var moduleArgs = {
+          wasmBinary: binary,
+          onRuntimeInitialized: function () {
+            console.log('initialized');
+          }
+        };
+        ModuleHogeInstance = ModuleHoge(moduleArgs);
+        ModuleHogeInstance.then((m) => {
+          this.instance = m;
+        });
+      });
   }
 
-  click2():void {
+  click2(): void {
     console.log('押されたよ2');
     //console.log(this.instance);
 
@@ -49,37 +42,34 @@ export class AppComponent {
       'number', // return type
       ['number', 'number'], // argument types
       [3, 9] // parameters
-  );
-  console.log(ret);
+    );
+    console.log(ret);
 
-  var ret2 = this.instance.ccall(
-    'get_length', // function name
-    'number', // return type
-    ['string'], // argument type
-    ['Hello World!'] // parameter
-);
-console.log(ret2);
+    var ret2 = this.instance.ccall(
+      'get_length', // function name
+      'number', // return type
+      ['string'], // argument type
+      ['Hello World!'] // parameter
+    );
+    console.log(ret2);
   }
 
-  multiply(a: number, b: number): number
-  {
+  multiply(a: number, b: number): number {
     var ret = Module.ccall(
-        'multiply', // function name
-        'number', // return type
-        ['number', 'number'], // argument types
-        [a, b] // parameters
+      'multiply', // function name
+      'number', // return type
+      ['number', 'number'], // argument types
+      [a, b] // parameters
     );
-
     return ret;
   }
 
-  getLength(s: string): number
-  {
+  getLength(s: string): number {
     var ret = Module.ccall(
-        'get_length', // function name
-        'number', // return type
-        ['string'], // argument type
-        [s] // parameter
+      'get_length', // function name
+      'number', // return type
+      ['string'], // argument type
+      [s] // parameter
     );
     return ret;
   }
